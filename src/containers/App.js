fde0e561548e516ec4b5eb5c9e4116de56099b39
Nicole Gibson <nikkig124@gmail.com>
2020-02-09 14:53:17 -0600

    init

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/SearchBox.js';
+import './App.css';
+import Scroll from '../components/Scroll.js'
+
+//smart component because of state
+class App extends Component{
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
+  
+  }
+
+  onSearchChange = (event) => {
+    //when there is a change run function
+    this.setState({searchField: event.target.value})
+  }
+
+  render(){
+    const { robots, searchField} = this.state;
+    //update state to what matches the searchfield
+    const filteredRobots = robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    //if there are no robots
+    return !robots.length ?
+    //show loading
+      <h1>Loading....</h1> :
+      //else
+        (
+        <div className = 'tc'>
+          <h1 className = 'f1'>Robo Friends</h1>
+          <SearchBox searchChange = {this.onSearchChange} />
+          <Scroll>
+            <CardList robots = {filteredRobots}/>
+          </Scroll>
+        </div>
+        );
+    
+  }
+}
+export default App;
