9373f0498c5e3d7c9d1ebae741e353f274580d2d
Nicole Gibson <nikkig124@gmail.com>
2020-02-10 09:19:01 -0600

    changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList.js';
 import SearchBox from '../components/SearchBox.js';
 import './App.css';
-import Scroll from '../components/Scroll.js'
+import Scroll from '../components/Scroll.js';
+import ErrorBoundary from '../components/ErrorBoundary.js';
 
 //smart component because of state
 class App extends Component{
@@ -42,7 +43,9 @@ class App extends Component{
           <h1 className = 'f1'>Robo Friends</h1>
           <SearchBox searchChange = {this.onSearchChange} />
           <Scroll>
-            <CardList robots = {filteredRobots}/>
+            <ErrorBoundary>
+              <CardList robots = {filteredRobots}/>
+            </ErrorBoundary>
           </Scroll>
         </div>
         );
